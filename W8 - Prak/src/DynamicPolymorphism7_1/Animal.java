ca5538245c571ca5f74490bc5662366e23da1366
iqbalalghifary <iqbalalghifary97@gmail.com>
2021-11-12 08:58:03 +0700

    rebes
    rebess

---
+++ W8 - Prak/src/DynamicPolymorphism7_1/Animal.java
@@ -0,0 +1,8 @@
+package DynamicPolymorphism7_1;
+
+public class Animal {
+    public void sound() {
+        System.out.println("Animal is making a sound");
+    }
+}
+
---
+++ W8 - Prak/src/DynamicPolymorphism7_1/Horse.java
@@ -0,0 +1,15 @@
+package DynamicPolymorphism7_1;
+
+public class Horse extends Animal {
+    public void sound() {
+        System.out.println("Neigh");
+    }
+
+
+	public static void main(String[] args) {
+		Animal obj=new Horse();
+		obj.sound();
+	}
+}
+
+
